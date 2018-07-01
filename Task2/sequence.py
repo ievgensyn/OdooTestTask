83423a388561b1767a9f3bc1f67985ce4b007050
ievgen <yevgens@web.de>
2018-07-02 00:39:57 +0300

    third commit

--- Task2/sequence.py
+++ Task2/sequence.py
@@ -5,5 +5,15 @@ import string
 random = ''.join([random.choice(string.ascii_lowercase) for i in xrange(1000000)])
 
 
-count = random.count(raw_input())
-print count
+print random
+print ''
+
+# for testing
+x = 'j'
+b = 'll'
+y = 'zzz'
+
+count1 = random.count(x)
+count2 = random.count(b)
+count3 = random.count(y)
+print count1, count2, count3
