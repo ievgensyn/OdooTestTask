f8a78efaf4da4255d5df7abc5f9bdda2dfda74ca
ievgen <yevgens@web.de>
2018-07-01 12:37:50 +0300

    task2

---
+++ Task2/sequence.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+
+random = ''.join([random.choice(string.ascii_lowercase) for i in xrange(1000000)])
+
+# print random
+# print len(random)
+
+count = random.count(raw_input())
+print count
