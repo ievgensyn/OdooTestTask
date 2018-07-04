db427d6396c9bcb5f3c7b36ff39d6929c174524c
ievgen <yevgens@web.de>
2018-07-04 09:15:43 +0300

    second commit

--- Task3/order.py
+++ Task3/order.py
@@ -1,6 +1,6 @@
-vat = int(raw_input('Enter the VAT: ')) / 100
+vat = float(raw_input('Enter the VAT: '))
 a = {397.01:1, 435.0:2, 435.0:2, 443.33:2, 443.33:2, 370.0:2, 630.0:1, 630.0:1, 630.0:2}
-b = [[i, a[i]] for i in a]
+#b = [[i, a[i]] for i in a]
 
 sum_item = [i * a[i] for i in a]
 print sum_item
@@ -9,6 +9,7 @@ def sum_total(numList):
     if len(numList) == 1:
         return numList[0]
     else:
-        return numList[0] + sum_item_total(numList[1:])
+        return numList[0] + sum_total(numList[1:])
 
-print sum_total(sum_item)
+print vat
+print sum_total(sum_item) / vat
